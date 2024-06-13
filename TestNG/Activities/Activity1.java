332167428bf18f99fc0c28a7dbfe3fc2021deea0
umasankarpurighalla <upurigha@in.ibm.com>
2024-06-13 11:19:01 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity1.java
@@ -0,0 +1,52 @@
+package activities;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Activity1 {
+    // Declare the WebDriver object
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeMethod() {
+        // Set up the Firefox driver
+        WebDriverManager.firefox().setup();
+        //Create a new instance of the Firefox driver
+        driver = new FirefoxDriver();
+
+        //Open browser
+        driver.get("https://v1.training-support.net");
+    }
+
+    @Test
+    public void exampleTestCase() {
+        // Check the title of the page
+        String title = driver.getTitle();
+
+        //Print the title of the page
+        System.out.println("Page title is: " + title);
+
+        //Assertion for page title
+        Assert.assertEquals("Training Support", title);
+
+        //Find the clickable link on the page and click it
+        driver.findElement(By.id("about-link")).click();
+
+        //Print title of new page
+        System.out.println("New page title is: " + driver.getTitle());
+
+        Assert.assertEquals(driver.getTitle(), "About Training Support");
+    }
+
+    @AfterClass
+    public void afterMethod() {
+        //Close the browser
+        driver.quit();
+    }
+}
